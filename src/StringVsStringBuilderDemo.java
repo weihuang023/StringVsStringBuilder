3c13cbcab69088dd4c4a5b5616aa30591b8c3b1f
weihuang023 <weih81888@gmail.com>
2016-02-03 18:22:07 -0500

    Initial Commit

---
+++ src/StringVsStringBuilderDemo.java
@@ -0,0 +1,38 @@
+
+public class StringVsStringBuilderDemo {
+
+	    public static void main(String[] args) {
+
+	    	// StringBuilder objects are like String objects, except that they can be modified.
+	    	
+	    	String palindrome = "Dot saw I was Tod";
+	        StringBuilder sb = new StringBuilder(palindrome);
+	        
+	        sb.reverse();  // reverse it
+	        
+	        System.out.println(sb);
+	        
+	        int len = palindrome.length();
+			char[] tempCharArray = new char[len];
+	        char[] charArray = new char[len];
+	        
+	        // put original string in an 
+	        // array of chars
+	        for (int i = 0; i < len; i++) {
+	            tempCharArray[i] = 
+	                palindrome.charAt(i);
+	        } 
+	        
+	        // reverse array of chars
+	        for (int j = 0; j < len; j++) {
+	            charArray[j] =
+	                tempCharArray[len - 1 - j];
+	        }
+	        
+	        String reversePalindrome =
+	            new String(charArray);
+	        System.out.println(reversePalindrome);
+	        
+	    }
+	}
+
